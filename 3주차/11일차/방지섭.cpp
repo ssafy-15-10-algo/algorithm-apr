633c1c4b8b6b286c6d34505c7bb493fa2f262ccd
방지섭 <bjs0306b@gmail.com>
2026-04-16 22:03:21 +0900

    BOJ 20440 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1/방지섭

--- 3주차/11일차/방지섭.cpp
+++ 3주차/11일차/방지섭.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n; 
-int room_cnt[100001];
+int computer_cnt[100001];
 vector<pair<int,int>> v;
 void input(){
     cin >> n;
@@ -15,27 +15,27 @@ void input(){
 }
 
 void solve(){
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq1; // 현재 진행 중인 스케쥴 <끝나는 시간, 방 idx>
-    priority_queue<int, vector<int>, greater<int>> pq2; // 현재 사용 가능한(스케쥴이 끝난) 가장 작은 index를 고르는 용도
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq1; // 현재 사용 중인 컴퓨터 <끝나는 시간, 방 idx>
+    priority_queue<int, vector<int>, greater<int>> pq2; // 현재 사용 가능한(스케쥴이 끝난) 가장 작은 index를 가진 컴퓨터를 고르는 용도
 
-    int room_num = 0;
+    int computer_idx = 0;
     for(int i=0;i<n;i++){
-        while(!pq1.empty() && pq1.top().first <= v[i].first){ // 현재 v[i]의 입장 시간에 대해서 이미 스케쥴이 다 끝난 방을 pq2에 옮기기. 
+        while(!pq1.empty() && pq1.top().first <= v[i].first){ // 현재 v[i]의 입장 시간에 대해서 이미 사용이 다 끝난 컴퓨터를 pq2에 옮기기. 
             pq2.push(pq1.top().second); pq1.pop();
         }
 
-        if(pq2.empty()){ // 만약 스케쥴이 다 끝난 방이 하나도 없으면 pq1에 새로운 방을 추가해야 함.
-            pq1.push({v[i].second, room_num});
-            room_cnt[room_num++]++;
+        if(pq2.empty()){ // 만약 사용이 다 끝난 컴퓨터가 하나도 없으면 pq1에 새로운 방을 추가해야 함.
+            pq1.push({v[i].second, computer_idx});
+            computer_cnt[computer_idx++]++;
         }
-        else{ // 스케쥴이 다 끝난 방이 있으면 그 중에 가장 idx가 낮은 방을 선택해서 현재 진행 중인 스케쥴에 넣음.
-            room_cnt[pq2.top()]++;
+        else{ // 사용이 다 끝난 컴퓨터가 있으면 그 중에 가장 idx가 낮은 컴퓨터를 선택해서 현재 진행 중인 스케쥴(pq1)에 넣음.
+            computer_cnt[pq2.top()]++;
             pq1.push({v[i].second, pq2.top()});
             pq2.pop();
         }
     }
-    cout << room_num << "\n";
-    for(int i=0;i<room_num;i++) cout << room_cnt[i] << " ";
+    cout << computer_idx << "\n";
+    for(int i=0;i<computer_idx;i++) cout << computer_cnt[i] << " ";
 }
 
 int main(){
